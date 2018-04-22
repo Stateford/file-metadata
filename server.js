45629569379daf1f2b153b7a007a1bf4eab6955e
idietmoran <idietmoran@gmail.com>
2018-04-22 17:09:39 -0400

    finish

--- server.js
+++ server.js
@@ -2,33 +2,41 @@
 //
 // MODULES
 // ===============
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var path = require('path');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var multer = require('multer');
-var upload = multer();
-
-// CONFIG
-// ==================
-// load the config fies
-var hostDB = require('./config/host');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+//const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const multer = require("multer");
+const upload = multer();
+const app = express();
 
 // set up app
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(morgan("dev"));
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
 
-// routes
-// ===============
-
-require('./app/routes')(app);
 
 // server static files
-app.use(express.static(__dirname + '/public'));
+app.use("/", express.static(path.join(__dirname + "public")));
+
+app.get("/", function(req, res) {
+    console.log(`${__dirname}/public/index.html`);
+    res.sendFile(`${__dirname}/public/index.html`);
+});
+
+
+
+app.post("/", upload.single('file'), function(req, res) {
+
+    try {
+        res.json({ bytes :req.file.size});
+    } catch(e) {
+        res.send("Could not get file size");
+    }
+
+});
 
 // start server
-app.listen(host.port);
-console.log('server is listening to port: ' + host.port);
+app.listen(process.env.PORT);
+console.log("server is listening to port: " + process.env.PORT);
